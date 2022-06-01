9e393c00b0844b4a87ce47accd973b4116709c07
Aleksey Serdtsev <sng.player@gmail.com>
2022-06-01 19:32:26 +0300

    Refactored PlayerSlot into separate file

---
+++ src/components/PlayerSlot/index.tsx
@@ -0,0 +1,75 @@
+import { Player } from "interfaces/Player";
+import styles from "./styles.module.scss";
+import { lobbyWs } from "api";
+import { Editable } from "../Editable";
+import { getUser, updateUser } from "ls";
+import { ReactComponent as PictureSvg } from "svg/picture.svg";
+
+interface PlayerSlotProps {
+    player: Player;
+}
+
+export const PlayerSlot = ({ player }: PlayerSlotProps) => {
+    const me = getUser().id === player.id;
+
+    const name = player.name ? player.name : "[Имя не задано]";
+    const nameStyles = [styles.name];
+    if (me) nameStyles.push(styles.editable);
+
+    const secret = player.secret ? player.secret : "[Кто он/она?]";
+    const secretStyles = [styles.secret];
+    if (!me) secretStyles.push(styles.editable);
+
+    const onUpdate = (field: string, value: string) => {
+        lobbyWs.emit("player/change", { id: player.id, field, value });
+    };
+
+    const onChangePicture = () => {
+        const value = prompt("Полный url картинки (https://*.*/***/image.jpg)");
+        lobbyWs.emit("player/change", { id: player.id, field: "picture", value });
+    };
+
+    return (
+        <div className={styles.slot}>
+            <span className={nameStyles.join(" ")}>
+            {
+                me ? <Editable
+                        value={name}
+                        onUpdate={(value) => {
+                            const user = getUser();
+                            user.name = value;
+                            updateUser(user);
+                            onUpdate("name", value)
+                        }}
+                    /> :
+                    <div className={styles.nameDetail}>
+                        <PictureSvg onClick={onChangePicture}/>
+                        <span>{name}</span>
+                    </div>
+            }
+            </span>
+            <span className={secretStyles.join(" ")}>
+                {
+                    !me && <>
+                        <Editable
+                            value={secret}
+                            onUpdate={(value) => onUpdate("secret", value)}
+                        />
+                    </>
+                }
+            </span>
+            <div className={styles.image}>
+                {
+                    !me && <>
+                        {player.picture &&
+                            <img src={player.picture}
+                                 alt="подсказка"
+                                //onClick={}
+                            />
+                        }
+                    </>
+                }
+            </div>
+        </div>
+    );
+};
--- src/components/Players/index.tsx
+++ src/components/Players/index.tsx
@@ -1,86 +1,13 @@
 import React from "react";
-import { lobbyWs } from "api";
-import { getUser, updateUser } from "ls";
 import { Player } from "interfaces/Player";
-import { Editable } from "../Editable";
-import { ReactComponent as PictureSvg } from "svg/picture.svg";
+import { PlayerSlot } from "../PlayerSlot";
 import styles from "./styles.module.scss";
-
-interface PlayerSlotProps {
-    player: Player;
-    me: boolean;
-}
-
-const PlayerSlot = ({ player, me }: PlayerSlotProps) => {
-    const name = player.name ? player.name : "[Имя не задано]";
-    const nameStyles = [styles.name];
-    if (me) nameStyles.push(styles.editable);
-
-    const secret = player.secret ? player.secret : "[Кто он/она?]";
-    const secretStyles = [styles.secret];
-    if (!me) secretStyles.push(styles.editable);
-
-    const onUpdate = (field: string, value: string) => {
-        lobbyWs.emit("player/change", { id: player.id, field, value });
-    };
-
-    const onChangePicture = () => {
-        const value = prompt("Полный url картинки (https://*.*/***/image.jpg)");
-        lobbyWs.emit("player/change", { id: player.id, field: "picture", value });
-    };
-
-    return (
-        <div className={styles.slot}>
-            <span className={nameStyles.join(" ")}>
-            {
-                me ? <Editable
-                        value={name}
-                        onUpdate={(value) => {
-                            const user = getUser();
-                            user.name = value;
-                            updateUser(user);
-                            onUpdate("name", value)
-                        }}
-                    /> :
-                    <div className={styles.nameDetail}>
-                        <PictureSvg onClick={onChangePicture}/>
-                        <span>{name}</span>
-                    </div>
-            }
-            </span>
-                <span className={secretStyles.join(" ")}>
-                {
-                    !me && <>
-                        <Editable
-                            value={secret}
-                            onUpdate={(value) => onUpdate("secret", value)}
-                        />
-                    </>
-                }
-            </span>
-            <div className={styles.image}>
-            {
-                !me && <>
-                    {player.picture &&
-                        <img src={player.picture}
-                             alt="подсказка"
-                             //onClick={}
-                        />
-                    }
-                </>
-            }
-            </div>
-        </div>
-    );
-};
 
 interface PlayersProps {
     players: Player[];
 }
 
 export const Players = ({ players }: PlayersProps) => {
-    const user = getUser();
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.players}>
@@ -89,7 +16,6 @@ export const Players = ({ players }: PlayersProps) => {
                         <PlayerSlot
                             key={player.id}
                             player={player}
-                            me={user.id === player.id}
                         />
                     )
                 }
