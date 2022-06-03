0b080937d6cc9bc48be30f04842778bda3968ed1
Aleksey Serdtsev <sng.player@gmail.com>
2022-06-03 12:45:29 +0300

    Added rolling dices sound

--- src/components/RollPlayers/index.tsx
+++ src/components/RollPlayers/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { lobbyWs } from "api";
 import { ReactComponent as DiceSvg } from "svg/dices.svg";
 import styles from "./styles.module.scss";
 
 export const RollPlayers = () => {
+    const [disabled, setDisabled] = useState(false);
+    const rollSound = new Audio("/sounds/dices.mp3");
+    const buttonStyles = [styles.button];
+    if (disabled) buttonStyles.push(styles.disabled);
+
+    const roll = () => {
+        if (disabled) return;
+        lobbyWs.emit("player/roll");
+        if (rollSound) {
+            setDisabled(true);
+            rollSound.play();
+            setTimeout(() => setDisabled(false), 700);
+        }
+    };
+
     return (
         <div className={styles.wrapper}>
             <DiceSvg
-                className={styles.button}
-                onClick={() => lobbyWs.emit("player/roll")}
+                className={buttonStyles.join(" ")}
+                onClick={roll}
             />
         </div>
     );
