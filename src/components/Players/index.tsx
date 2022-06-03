de5a0c81557530cd9643d45062cc00f0369847c2
Aleksey Serdtsev <sng.player@gmail.com>
2022-06-03 11:50:50 +0300

    Added react-spring lib; Added players shuffle animation

--- src/components/Players/index.tsx
+++ src/components/Players/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTransition, animated } from "react-spring";
 import { Player } from "interfaces/Player";
 import { PlayerSlot } from "../PlayerSlot";
 import styles from "./styles.module.scss";
@@ -8,17 +9,36 @@ interface PlayersProps {
 }
 
 export const Players = ({ players }: PlayersProps) => {
+    const width = 220;
+    const gap = 20;
+    const totalWidth = players.length * 220 + (players.length - 1) * gap;
+    const transitions = useTransition(
+        players.map((player, i) => ({ ...player, x: i * width + i * gap - totalWidth / 2 })),
+        {
+            from: { position: "absolute" },
+            leave: { width: 0 },
+            enter: ({ x }) => ({ x }),
+            update: ({ x }) => ({ x }),
+            key: (item: Player) => item?.id
+        }
+    );
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.players}>
-                {
-                    players.map(player =>
+                {transitions((style, player) =>
+                    <animated.div
+                        //@ts-ignore
+                        style={{
+                            ...style
+                        }}
+                    >
                         <PlayerSlot
                             key={player.id}
                             player={player}
                         />
-                    )
-                }
+                    </animated.div>
+                )}
             </div>
         </div>
     );
